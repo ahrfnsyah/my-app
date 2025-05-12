4d97f939130efe95b8416df1f70ae2a9789dfbc0
Clausensan <ahmadhuseinrifansyah@gmail.com>
2025-05-12 13:51:01 +0700

    try

--- pages/components/Articles/ArticlesCarousel.tsx
+++ pages/components/Articles/ArticlesCarousel.tsx
@@ -1,7 +1,7 @@
 // ArticlesCarousel.tsx
 import React from 'react';
 import Image from 'next/image';
-import styles from '@/pages/dashboard/dashboard.module.css';
+import styles from '@/styles/dashboard.module.css';
 
 interface Article {
   date: string;
