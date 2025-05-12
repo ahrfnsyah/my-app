679176772488a6341b5df1fcb6c4fccea34d794f
Clausensan <ahmadhuseinrifansyah@gmail.com>
2025-05-12 15:06:18 +0700

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}> Vegenation </h1>
-      <p className={styles.title1}> see your future commodity price prediction </p>
+      <p className={styles.title1}> see your future commodity price predictionnn </p>
       <p className={styles.title1}> and read related articles </p>
 
       <Link href="./dashboard" className={styles.button}>Go to Dashboard</Link>
